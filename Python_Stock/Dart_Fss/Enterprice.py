3ff11a8ed16c853d08205376995c09fe20864eea
SeungMin <75976324+SeungMin2001@users.noreply.github.com>
2024-05-18 16:33:31 +0900

    Dart-fss New class

---
+++ Python_Stock/Dart_Fss/Enterprice.py
@@ -0,0 +1,24 @@
+import dart_fss as dart
+class Main():
+    def __init__(self):
+        self.api_key = '2c62c26d0182d996a0bad19b99513c5db41cc4df'
+        dart.set_api_key(api_key=self.api_key)
+        self.api_key = dart.get_api_key()
+        self.corp_code = dart.api.filings.get_corp_code()
+        self.find_name()
+
+    def find_name(self):
+        for index in self.corp_code:
+            if index['corp_name'] == '삼성전자':
+                print(index['corp_code'])
+                return True
+
+
+if __name__=="__main__": #메인 실행파일 명시
+    Main()
+
+        
+
+
+        
+
